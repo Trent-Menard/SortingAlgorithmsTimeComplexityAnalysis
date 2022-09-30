41249e602566c0e7c4a8f22729c40d014f52d803
Trent Menard <trentmenard6@yahoo.com>
2022-09-30 15:39:40 -0500

    Create separate .py files for each sorting algorithm

---
+++ BubbleSort.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 30 15:29:30 2022
+
+@author: DJMack, Trent
+"""
+
+#Bubble sort algorithm
+def bubble_sort(arr):
+    def swap(i, j):
+        arr[i], arr[j] = arr[j], arr[i]
+
+    n = len(arr)
+    swapped = True
+    
+    x = -1
+    while swapped:
+        swapped = False
+        x = x + 1
+        for i in range(1, n-x):
+            if arr[i - 1] > arr[i]:
+                swap(i - 1, i)
+                swapped = True
+                    
+    return arr
