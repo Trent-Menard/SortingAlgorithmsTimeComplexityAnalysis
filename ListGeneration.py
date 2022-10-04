bb572584551943a76ea57b5ff508b991e2f0c128
Trent Menard <trentmenard6@yahoo.com>
2022-10-03 22:45:23 -0500

    Create ListGeneration.py & update Radix Sort code to implement it

---
+++ ListGeneration.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  3 22:01:34 2022
+
+@author: Trent
+"""
+
+import random
+
+# Generates new list of integers each time this object is created.
+# Used for testing various sorting algorithms in Main.py.
+class ListGeneration:
+    def __init__(self):
+        # Since all same, can copy values from prev list rather than re-gen.
+        self.List_100_Ascending = [x for x in range(100)]
+        self.List_1K_Ascending = list.copy(self.List_100_Ascending)
+        self.List_1K_Ascending += [x for x in range(1_000, 10_000)]
+        self.List_10K_Ascending = list.copy(self.List_1K_Ascending)
+        self.List_10K_Ascending += [x for x in range(1_000, 10_000)]
+        
+        self.List_100_Random = [random.randint(0, 100) for x in range(100)]
+        self.List_1K_Random = [random.randint(0, 1_000) for x in range(0, 1_000)]
+        self.List_10K_Random = [random.randint(0, 10_000) for x in range(0, 10_000)]
+        
+        self.List_100_Reversed = [x for x in reversed(range(100))]
+        self.List_1K_Reversed = [x for x in reversed(range(0, 1_000))]
+        self.List_10K_Reversed = [x for x in reversed(range(0, 10_000))]
--- Main.py
+++ Main.py
@@ -10,6 +10,8 @@ from BubbleSort import bubble_sort
 from MergeSort import merge_sort
 from RadixSort import radixSort
 
+from ListGeneration import ListGeneration
+
 import random
 from time import perf_counter
 
@@ -20,83 +22,59 @@ Test_100_Result = []
 Test_1K_Result = []
 Test_10K_Result = []
 
-# Ascending lists (for already sorted data test)
-Test_100 = [x for x in range(100)]
+rdx = ListGeneration()
 
-Test_1K = list.copy(Test_100)
-Test_1K += [x for x in range(100, 1_000)]
-
-Test_10K = list.copy(Test_1K)
-Test_10K += [x for x in range(1_000, 10_000)]
-
-# Test Radix sort BEST case: Ω(n+k)
+# Test Radix sort BEST case (already sorted): Ω(n+k)
 print("Running BEST Case for RADIX Sort:")
 start = perf_counter()
-Test_100_Result = radixSort(Test_100)
+Test_100_Result = radixSort(rdx.List_100_Ascending)
 stop = perf_counter()
 print("  Size:\t\t\t\tTime Elapsed:")
 print("  100\t\t\t\t" + str(stop - start))
 
 start = perf_counter()
-radixSort(Test_1K)
+Test_100_Result = radixSort(rdx.List_1K_Ascending)
 stop = perf_counter()
 print("  1,000\t\t\t\t" + str(stop - start))
 
 start = perf_counter()
-radixSort(Test_10K)
+Test_10K_Result = radixSort(rdx.List_10K_Ascending)
 stop = perf_counter()
 print("  10,000\t\t\t" + str(stop - start))
 
 
-
-# Random numbers list (for unsorted data test)
-print("\n*Generating random data, one moment please...*\n")
-Test_100 = [random.randint(0, 100) for x in range(100)]
-
-Test_1K = [random.randint(0, 1_000) for x in range(0, 1_000)]
-
-Test_10K = [random.randint(0, 10_000) for x in range(0, 10_000)]
-
-# Test Radix sort AVERAGE case: θ(nk)
+# Test Radix sort AVERAGE (random) case: θ(nk)
 print("Running AVERAGE Case for RADIX Sort:")
 start = perf_counter()
-Test_100_Result = radixSort(Test_100)
+Test_100_Result = radixSort(rdx.List_100_Random)
 stop = perf_counter()
 print("  Size:\t\t\t\tTime Elapsed:")
 print("  100\t\t\t\t" + str(stop - start))
 start = perf_counter()
-Test_1K_Result = radixSort(Test_1K)
+Test_1K_Result = radixSort(rdx.List_1K_Random)
 stop = perf_counter()
 print("  1,000\t\t\t\t" + str(stop - start))
 
 start = perf_counter()
-Test_10K_Result = radixSort(Test_10K)
+Test_10K_Result = radixSort(rdx.List_10K_Random)
 stop = perf_counter()
 print("  10,000\t\t\t" + str(stop - start))
 
-
-# Reverse numbers list (for worst case sorted data test)
-print("\n*Generating random data, one moment please...*\n")
-Test_100 = [random.randint(0, 100) for x in reversed(range(100))]
-
-Test_1K = [random.randint(0, 1_000) for x in reversed(range(0, 1_000))]
-
-Test_10K = [random.randint(0, 10_000) for x in reversed(range(0, 10_000))]
 
 # Test Radix sort WORST case O(nk):
 print("Running WORST Case for RADIX Sort:")
 start = perf_counter()
-Test_100_Result = radixSort(Test_100)
+Test_100_Result = radixSort(rdx.List_100_Random)
 stop = perf_counter()
 print("  Size:\t\t\t\tTime Elapsed:")
 print("  100\t\t\t\t" + str(stop - start))
 start = perf_counter()
-Test_1K_Result = radixSort(Test_1K)
+Test_1K_Result = radixSort(rdx.List_1K_Random)
 stop = perf_counter()
 print("  1,000\t\t\t\t" + str(stop - start))
 
 start = perf_counter()
-Test_10K_Result = radixSort(Test_10K)
+Test_10K_Result = radixSort(rdx.List_10K_Random)
 stop = perf_counter()
 print("  10,000\t\t\t" + str(stop - start))
 
@@ -122,15 +100,15 @@ stop = perf_counter()
 print("  Size:\t\t\t\tTime Elapsed:")
 print("  100\t\t\t\t" + str(stop - start))
 
-start = perf_counter()
-bubble_sort(Test_1K)
-stop = perf_counter()
-print("  1,000\t\t\t\t" + str(stop - start))
+# start = perf_counter()
+# bubble_sort(Test_1K)
+# stop = perf_counter()
+# print("  1,000\t\t\t\t" + str(stop - start))
 
-start = perf_counter()
-bubble_sort(Test_10K)
-stop = perf_counter()
-print("  10,000\t\t\t" + str(stop - start))
+# start = perf_counter()
+# bubble_sort(Test_10K)
+# stop = perf_counter()
+# print("  10,000\t\t\t" + str(stop - start))
 
 # Random numbers list (for unsorted data test)
 print("\n*Generating random data, one moment please...*\n")
@@ -152,10 +130,10 @@ Test_1K_Result = bubble_sort(Test_1K)
 stop = perf_counter()
 print("  1,000\t\t\t\t" + str(stop - start))
 
-start = perf_counter()
-Test_10K_Result = bubble_sort(Test_10K)
-stop = perf_counter()
-print("  10,000\t\t\t" + str(stop - start))
+# start = perf_counter()
+# Test_10K_Result = bubble_sort(Test_10K)
+# stop = perf_counter()
+# print("  10,000\t\t\t" + str(stop - start))
 
 # Reverse numbers list (for worst case sorted data test)
 print("\n*Generating random data, one moment please...*\n")
