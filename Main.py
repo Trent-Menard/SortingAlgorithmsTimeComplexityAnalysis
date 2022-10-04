70d23aecab8b6afdc59da837821dbed94f979994
Trent Menard <trentmenard6@yahoo.com>
2022-10-03 20:57:36 -0500

    Implement Worst Case for Radix sort

--- Main.py
+++ Main.py
@@ -56,10 +56,33 @@ Test_1K = [random.randint(0, 1_000) for x in range(0, 1_000)]
 
 Test_10K = [random.randint(0, 10_000) for x in range(0, 10_000)]
 
-print(Test_100)
-
 # Test Radix sort AVERAGE case: θ(nk)
 print("Running AVERAGE Case for RADIX Sort:")
+start = perf_counter()
+Test_100_Result = radixSort(Test_100)
+stop = perf_counter()
+print("  Size:\t\t\t\tTime Elapsed:")
+print("  100\t\t\t\t" + str(stop - start))
+start = perf_counter()
+Test_1K_Result = radixSort(Test_1K)
+stop = perf_counter()
+print("  1,000\t\t\t\t" + str(stop - start))
+
+start = perf_counter()
+Test_10K_Result = radixSort(Test_10K)
+stop = perf_counter()
+print("  10,000\t\t\t" + str(stop - start))
+
+# Reverse numbers list (for worst case sorted data test)
+print("\n*Generating random data, one moment please...*\n")
+Test_100 = [random.randint(0, 100) for x in reversed(range(100))]
+
+Test_1K = [random.randint(0, 1_000) for x in reversed(range(0, 1_000))]
+
+Test_10K = [random.randint(0, 10_000) for x in reversed(range(0, 10_000))]
+
+# Test Radix sort WORST case O(nk):
+print("Running WORST Case for RADIX Sort:")
 start = perf_counter()
 Test_100_Result = radixSort(Test_100)
 stop = perf_counter()
